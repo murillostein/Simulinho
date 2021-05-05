0cdfbc492cb126620028f9eba45b457d60a35d97
Murillo Stein <murillo.stein8@gmail.com>
2021-05-05 10:13:07 -0300

    atualizei tudo

--- SIMULINHO/simulinho-html-alunos.py
+++ SIMULINHO/simulinho-html-alunos.py
@@ -13,6 +13,7 @@ def _color_correction(data):
 
 def _color_table_every_other(data):
     df = data.copy()
+    # print(len(df))
     n = len(df)
     df.iloc[range(0,n,2), :] = 'background-color: #f5f5dc'
     df.iloc[range(1,n,2), :] = 'background-color: #f0f0ce'
@@ -33,6 +34,7 @@ df_n['Media Geral'] = df_n['Media Geral'].apply(lambda x: str(round(x,3)*100)+'%
 # df_g = pd.read_pickle('df_notas_geral.pkl').rename(columns={'Nota':'Média geral'})
 # df_g['Média geral'] = df_g['Média geral'].apply(lambda x: str(round(x,1))+'%')
 
+# dados da redacao
 df_r = pd.read_pickle('dados-relatorio-alunos\dados_redacao.pkl')
 
 relatorio_alunos = {}
@@ -55,12 +57,38 @@ for aluno in df_q.index.get_level_values(0).unique():
     # a cada iteração, ele pega os dados correspondente ao 'aluno'
     doc['nome'] = aluno # nome do aluno
     doc['materias'] = []
+
+
+    # dados pessoais de cada aluno
+
+    df_pessoal = pd.read_pickle("dados-relatorio-alunos\dados_pessoais.pkl")
+
+    df_pessoal = df_pessoal.set_index('nome')
+    df_pessoal = df_pessoal.loc[aluno, :]
+    df_pessoal = df_pessoal.to_frame().transpose()
+   
+    doc['pessoal'] = df_pessoal.style.apply(
+                _color_table_every_other,
+                axis=None
+            ).set_properties(
+                **{'text-align':'center',
+                   'font-family':'Roboto',
+                   'border-color':'black',
+                   'border-style' :'solid',
+                   'border-width': '1px',
+                   'border-collapse':'collapse'}
+            ).set_table_styles(
+                [{'props':[('color','#533884'), ('font-family','Roboto')]}]
+            ).hide_index().render()
+
+
+    # questoes de cada aluno
     df_q_aluno = df_q.loc[aluno, :]
+    
+        
+    df_q_aluno['Questão'] = df_q_aluno.index.get_level_values(1)
     # print(df_q_aluno)
-    
-    
-    df_q_aluno['questao'] = df_q_aluno.index.get_level_values(1)
-    df_q_aluno = df_q_aluno[['questao','Sua resposta','Gabarito','assunto']]
+    df_q_aluno = df_q_aluno[['Questão','Sua resposta','Gabarito','assunto', 'dificuldade']]
     
     # pra cada matéria:
     for materia in df_q_aluno.index.get_level_values(0).unique():
@@ -84,22 +112,18 @@ for aluno in df_q.index.get_level_values(0).unique():
 
     
 
-
+    # mostrar a pontuação geral em cada materia
 
     df_n_aluno = df_n.loc[aluno, :]
-    df_n_aluno['Matéria'] = df_n_aluno.index
+    df_n_aluno['Matéria'] = df_n_aluno.index.get_level_values(0)
     df_n_aluno.loc[:, 'Total de Acertos'] = df_n_aluno['Total de Acertos']# .apply(lambda x: str(int(x))) + '/' + df_n_aluno['Contagem'].apply(lambda x: str(int(x)))
-    df_n_aluno.loc[:, 'Média Individual'] = df_n_aluno['Média Individual']# .apply(lambda x: str(round(x,1))+'%')
+    df_n_aluno.loc[:, 'Média Individual'] = df_n_aluno['Média Individual']
     df_n_aluno.loc[:, 'Media Geral'] = df_n_aluno['Media Geral']
     
     
     df_n_aluno = df_n_aluno[['Matéria','Total de Acertos', 'Média Individual',  'Media Geral']]
-    ## print(df_n_aluno)
-    #vprint(df_n_aluno)
-    # a proxima linha não é necessária, o valor da média individual de cada aluno já vem com a media geral
-    #  df_n_aluno = df_n_aluno.join(df_g)
-
-
+    
+    #print(df_n_aluno)
 
     doc['notas'] = df_n_aluno.style.apply(
                 _color_table_every_other,
@@ -119,25 +143,35 @@ for aluno in df_q.index.get_level_values(0).unique():
 
 
     # redacao
-    # red_aluno = df_r.loc[aluno, :]
-    red_aluno = df_r.loc[df_r['nome'] == aluno]
-    red_aluno = red_aluno.drop(columns=['nome', 'cpf']) # .transpose()
-   #  print(doc['notas'])
-    doc['redacao'] = red_aluno
-    
-    redacao = red_aluno.to_dict()
-    print(redacao)
 
+    red_aluno = df_r.loc[aluno, :]
+    # print(len(red_aluno))
+    #)
+    #red_aluno.loc[:, 'Nota'] = red_aluno['Nota']
+    #red_aluno.loc[:, 'Comentario'] = red_aluno['Comentario']
     
-    # path pro grafico
-    # não tem gráfico para alunos
-    # doc['grafico_path'] = f'/home/pedro/einstein/simenem/graficos/total_{aluno.replace(" ","_")}.png'
+    #red_aluno= red_aluno[['Criterio', 'Nota','Comentario']]
+    # df_q_aluno = df_q_aluno[['Questão','Sua resposta','Gabarito','assunto', 'dificuldade']]
+    # red_aluno
+    print(red_aluno)
+    # redacao = red_aluno.to_dict()
+    #print(redacao)
+    doc['redacao'] = red_aluno.style.apply(
+                _color_table_every_other,
+                axis=None
+            ).set_properties(
+                **{'text-align':'center',
+                   'font-family':'Roboto',
+                   'border-color':'black',
+                   'border-style' :'solid',
+                   'border-width': '1px',
+                   'border-collapse':'collapse'}
+            ).set_table_styles(
+                [{'props':[('color','#533884'), ('font-family','Roboto')]}]
+            ).render()
     
-
+    
     # renderiza o html a partir do template e com as informações do dicionário
     html_out = template.render(doc)
     with open(f"html-alunos/{aluno.replace(' ', '_')}.html",'w') as file:
         file.write(html_out)
-
-
-
